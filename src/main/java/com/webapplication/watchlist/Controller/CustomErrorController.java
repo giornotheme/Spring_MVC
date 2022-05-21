efd368159a5a668f7482448a81dd428b138a1c37
giornotheme <vtang@outlook.fr>
2022-05-21 15:00:30 +0200

    better error page : display code error

--- src/main/java/com/webapplication/watchlist/Controller/CustomErrorController.java
+++ src/main/java/com/webapplication/watchlist/Controller/CustomErrorController.java
@@ -2,6 +2,7 @@ package com.webapplication.watchlist.Controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletResponse;
@@ -14,9 +15,10 @@ public class CustomErrorController implements ErrorController {
     }
 
     @GetMapping("/error")
-    public String handleError(HttpServletResponse response){
+    public String handleError(HttpServletResponse response, Model model){
         int code = response.getStatus();
-        System.out.println("Error with code" + code + "happened");
+        model.addAttribute("code", code);
+        System.out.println("Error with code " + code + " happened");
         return "error";
     }
 
